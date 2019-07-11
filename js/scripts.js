ba0e8d5eb9d6810b44e985b9116738e1ea94734b
Student <example@example.com>
2019-07-10 20:44:27 -0700

    just print on next page

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,39 @@
+//Business logic for Pizza Order----------
+function OrderList(){
+  this.orders = [],
+  this.orderId = 0
+}
+
+OrderList.prototype.addOrder = function(order){
+  order.orderId +=1;
+  return this.orderId;
+}
+
+function Order(pizzaToppings, pizzaSize){
+  this.pizzaToppings = pizzaToppings,
+  this.pizzaSize = pizzaSize
+
+}
+Order.prototype.pizzaOrder = function(){
+  return this.pizzaToppings + " " + this.pizzaSize;
+}
+function checkTotal(){
+
+}
 
 
 
 
-
+var orderList = new OrderList();
 
 $(document).ready(function(){
   $("form#order_pizza").submit(function(event){
     event.preventDefault();
+    var userResponces = [];
+    $("input:checkbox[name=pizza-topping]:checked").each(function(){
+      var pizzaToppingResquest = $(this).val();
+      userResponces.push(pizzaToppingResquest);
+    });
     $("#topping-responses").show();
     $("input:checkbox[name=pizza-topping]:checked").each(function(){
       var pizzaToppingResquest = $(this).val();
@@ -17,6 +44,7 @@ $(document).ready(function(){
       var pizzaSizeRequest = $(this).val();
       $('#size-responses').append(pizzaSizeRequest + "<br>");
     });
-    $('#orde_pizza').hide();
+
+    $('#order_pizza').hide();
   });
 });
